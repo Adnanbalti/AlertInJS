f5be7adeb4aba7dd552179ee9ec887ea560b24bf
Adnan Balti <adnanqasmi267@gmail.com>
2023-02-02 01:08:10 +0500

    alertJS

---
+++ app.js
@@ -0,0 +1,28 @@
+/*                 Question-01. 
+Write a script to greet your website visitor using JS alert
+box.*/
+alert("Hello! its Muhammad Adnan site");
+
+
+/*                 Question-02. 
+Write a script to display following message on your web
+page:*/
+alert("Error! Please enter a valid password.");
+
+
+/*3. Write a script to display following message on your web
+page: (Hint : Use line break)*/
+alert("Welcome to JS Land... \n Happy Coding");
+
+
+/*4. Write a script to display following messages in sequence:
+ALERTS | JAVASCRIPT
+Page 2 of 3*/
+alert("Welcome to JS Land...");
+
+alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+
+/*5. Generate the following message through browser’s
+developer console:*/
+alert("Hello... I can run JS through my web browser's console");
